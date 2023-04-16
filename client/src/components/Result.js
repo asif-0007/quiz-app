240dc375248062720209de213e95b60e0abbd7ed
Mohammad Asif <mohdasif4949@gmail.com>
2023-04-17 01:47:29 +0530

    made some changes to styles and components

--- client/src/components/Result.js
+++ client/src/components/Result.js
@@ -38,7 +38,7 @@ export default function Result() {
 
   return (
     <div className='container'>
-        <h1 className='title text-light'>Quiz Application</h1>
+        <h1 className='title text-light'>Quiz App</h1>
 
         <div className='result flex-center'>
             <div className='flex'>
